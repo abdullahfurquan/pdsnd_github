0cc98c7a19e6f0cf8cad7b837e59e9a9c8015bef
FURQUAN <abdufurq@amazon.com>
2023-04-09 09:07:02 +0530

    feat: adding file bikeshare.py

---
+++ bikeshare.py
@@ -0,0 +1,104 @@
+import time
+import pandas as pd
+import numpy as np
+
+CITY_DATA = { 'chicago': 'chicago.csv',
+              'new york city': 'new_york_city.csv',
+              'washington': 'washington.csv' }
+
+
+def get_filters():
+    """
+    Asks user to specify a city, month, and day to analyze.
+    Returns:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    """
+    
+
+
+def load_data(city, month, day):
+    """
+    Loads data for the specified city and filters by month and day if applicable.
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - Pandas DataFrame containing city data filtered by month and day
+    """
+    
+
+def trip_duration_stats(df):
+    """Displays statistics on the total and average trip duration."""
+
+    print('\nCalculating Trip Duration...\n')
+    start_time = time.time()
+
+    # display total travel time
+    total_duration = df['Trip Duration'].sum() / 3600.0
+    print("total travel time in hours is: ", total_duration)
+
+    # display mean travel time
+    mean_duration = df['Trip Duration'].mean() / 3600.0
+    print("mean travel time in hours is: ", mean_duration)
+
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+
+def user_stats(df):
+    """Displays statistics on bikeshare users."""
+
+    print('\nCalculating User Stats...\n')
+    start_time = time.time()
+
+    # Display counts of user types
+    user_types = df['User Type'].value_counts()
+    print(user_types)
+    # Display counts of gender
+    user_gender = df['Gender'].value_counts()
+    print(user_gender)
+
+
+    # Display earliest, most recent, and most common year of birth
+    earliest_year_of_birth = int(df['Birth Year'].min())
+    most_recent_year_of_birth = int(df['Birth Year'].max())
+    most_common_year_of_birth = int(df['Birth Year'].value_counts().idxmax())
+    print("The earliest year of birth is:",earliest_year_of_birth,
+          ", most recent one is:",most_recent_year_of_birth,
+           "and the most common one is: ",most_common_year_of_birth)
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+def raw_data (df):
+    """Displays the data due filteration.
+    5 rows will added in each press"""
+    print('press enter to see row data, press no to skip')
+    x = 0
+    while (input()!= 'no'):
+        x = x+5
+        print(df.head(x))
+
+
+def main():
+    while True:
+        city, month, day = get_filters()
+        df = load_data(city, month, day)
+
+        time_stats(df)
+        station_stats(df)
+        trip_duration_stats(df)
+        user_stats(df)
+        raw_data(df)
+
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
+
+
+if __name__ == "__main__":
+	main()
